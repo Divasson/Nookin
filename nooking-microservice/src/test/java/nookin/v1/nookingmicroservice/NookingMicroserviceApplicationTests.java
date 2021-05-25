25fdb121ae9f12502f4191b584ef9dcefad32312
Nacho Divasson <nacho@divasson.com>
2021-05-25 11:52:57 +0200

    Creación del Proyecto
    Conjunto de Carpetas Iniciales del proyecto

---
+++ nooking-microservice/src/test/java/nookin/v1/nookingmicroservice/NookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nookin.v1.nookingmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
