015a1bbab0c6a8fda91507bdb222b4995925d9cc
Alvarogg3 <Alvarogg3@users.noreply.github.com>
2021-05-27 13:01:36 +0200

    h

---
+++ nooking-microservice/src/main/java/nookin/v1/nookingmicroservice/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package nookin.v1.nookingmicroservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+	private String jwttoken;
+}
